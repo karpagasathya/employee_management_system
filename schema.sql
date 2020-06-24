e795d7fbf6b4c5d0dd0ad4820c96577ddf2172e2
sathya <karpagasathya@gmail.com>
2020-06-23 23:30:50 -0500

    schema.sql modified

--- schema.sql
+++ schema.sql
@@ -11,14 +11,14 @@ CREATE TABLE role (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    department_id INT
+    -- FOREIGN KEY (department_id) REFERENCES department(id)
 );
 CREATE TABLE employee (
     id int AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
-    manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id)
+    manager_id INT
+    -- FOREIGN KEY (role_id) REFERENCES role(id)
 );
